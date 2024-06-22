e4cdabee66e9447df189ef36584d14d9a54477ae
Haemin Park <haemin.parque@gmail.com>
2024-06-22 15:27:42 +0900

    [IP] - added dashboard page + feature to protect routes using the validateRequest()

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,6 +1,15 @@
+import { validateRequest } from "@/lib/validate-request";
 import Login from "./_component/Login";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+  const { user } = await validateRequest()
+
+  if (user) {
+    return redirect("/dashboard")
+  }
+
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
       <Login />
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { validateRequest } from "@/lib/validate-request"
+import { redirect } from "next/navigation"
+
+
+export default async function Page() {
+
+  const { user, session } = await validateRequest()
+
+  if (!user) {
+    return redirect("/")
+  }
+
+  return (
+    <div>
+      <p>Hello, {user.username}!</p>
+      <p>Your session expires in, {session.expiresAt.toString()}</p>
+    </div>
+  )
+}
